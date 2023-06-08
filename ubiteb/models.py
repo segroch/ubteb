7ab21194a8c41ca76eb2d17ab882e3dce79bcb4d
Agaba Claire Linda <agabaclaire70@gmail.com>
2023-06-08 11:27:15 +0300

    ubteb

--- ubiteb/models.py
+++ ubiteb/models.py
@@ -53,17 +53,12 @@ class Alumni(models.Model):
     completion_year = models.IntegerField(_('Completion Year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
     transcript_status = models.CharField(max_length=30, choices=TRANSCRIPT_CHOICES, default='received')
     certificate_status = models.CharField(max_length=30, choices=CERTIFICATE_CHOICES, default='received')
-    employment_status = models.CharField(max_length=10, choices=EMPLOYMENT_STATUS_CHOICES, default='employed')
-    employment_entity = models.CharField(max_length=20, choices=EMPLOYEMENT_ENTITY_CHOICES, default='government')
+    employment_status = models.CharField(max_length=40, choices=EMPLOYMENT_STATUS_CHOICES, default='employed')
+    employment_entity = models.CharField(max_length=40, choices=EMPLOYEMENT_ENTITY_CHOICES, default='government')
     
     
 
-    #mobile_num_regex = RegexValidator(regex="^[0-9]{10,15}$", message="Entered mobile number isn't in a right format!")
-    #parent_mobile_number = models.CharField(validators=[mobile_num_regex], max_length=13, blank=True)
 
-    
-    #others = models.TextField(blank=True)
-    #passport = models.ImageField(blank=True, upload_to="students/passports/")
         
 
     
